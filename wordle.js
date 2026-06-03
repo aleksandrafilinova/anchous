74cdb1e58527393fcd104c72180dd7299e82ca78
aleksandrafilinova <aleksandrafilinova322@gmail.com>
2026-06-03 18:24:28 +0800

    добавлен ввод с клавиатуры но проверка слова так и не осуществляется

--- wordle.js
+++ wordle.js
@@ -21,3 +21,40 @@ function initialize() {
     }
     
 }
+ document.addEventListener("keydown", processKey);
+
+
+
+function processKey(e) {
+    if (gameOver) return;
+
+    let key = e.key.toLowerCase();
+
+
+    if (/^[а-яё]$/.test(key)) {
+        if (col < width) {
+            let tile = document.getElementById(row + "-" + col);
+            tile.innerText = key.toUpperCase();
+            col++;
+        }
+    }
+    else if (key === "backspace") {
+        if (col > 0) {
+            col--;
+            let tile = document.getElementById(row + "-" + col);
+            tile.innerText = "";
+        }
+    }
+
+    else if (key === "enter") {
+        if (col !== width) return;
+
+        row++;
+        col = 0;
+
+        if (row === height) {
+            gameOver = true;
+            document.getElementById("answer").innerText = "Игра окончена";
+        }
+    }
+}
